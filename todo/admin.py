42d6fab0809082ef951f385c2477923e782ea824
Emre Kahraman <emrekahrman@gmail.com>
2024-08-01 00:31:41 +0300

    Models added to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from todo.models import Todo, Category, Tag
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'category',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_display_links = [
+        'id',
+        'title',
+        'category',
+        'is_active',
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'slug',
+        'is_active',
+    ]
+
+    list_display_links = [
+        'id',
+        'title',
+        'slug',
+        'is_active',
+    ]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'slug',
+    ]
+
+    list_display_links = [
+        'id',
+        'title',
+        'slug',
+    ]
+
+
+admin.site.register(Todo, TodoAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
